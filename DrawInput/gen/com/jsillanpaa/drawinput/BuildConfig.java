8a76ada80f111c31d001dc051abc6e0f7432fab1
Joonas Sillanpää <joonas.silla@gmail.com>
2012-10-06 17:53:23 +0300

    Initial commit

---
+++ DrawInput/gen/com/jsillanpaa/drawinput/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jsillanpaa.drawinput;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ DrawInput/src/com/jsillanpaa/drawinput/DrawInputMethodService.java
@@ -0,0 +1,38 @@
+package com.jsillanpaa.drawinput;
+
+import com.jsillanpaa.drawinput.R;
+
+import android.inputmethodservice.InputMethodService;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+
+public class DrawInputMethodService extends InputMethodService {
+	private static final String TAG = "DrawInputMethodService";
+	private View mContainerView;
+
+	@Override
+	public void onCreate() {
+		Log.i(TAG, TAG + ".onCreate()");
+		super.onCreate();
+
+	}
+
+	@Override
+	public View onCreateInputView() {
+		Log.i(TAG, TAG + ".onCreateInputView()");
+
+		mContainerView = (View) getLayoutInflater().inflate(
+				R.layout.drawinput_gui, null);
+
+		return mContainerView;
+
+	}
+
+	@Override
+	public void onStartInputView(EditorInfo info, boolean restarting) {
+		Log.i(TAG, TAG + ".onStartInputView(), restarting = " + restarting);
+		super.onStartInputView(info, restarting);
+	}
+
+}
